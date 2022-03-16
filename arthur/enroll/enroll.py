e071e2f61c80a6717f424149a6f9abc51ee09cd8
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-03-16 12:47:28 -0400

    Added new certificate script

--- arthur/enroll/enroll.py
+++ arthur/enroll/enroll.py
@@ -11,6 +11,11 @@ class Enrollment():
     node_system_id = None
     node = None
 
+    def __init__(self):
+        self.tenant_id = None
+        self.node_system_id = None
+        self.node = None
+
     def __init__(self, tenant_id, node_system_id):
         self.tenant_id = tenant_id
         self.node_system_id = node_system_id
@@ -23,7 +28,7 @@ class Enrollment():
         else:
             m = hashlib.sha256()
             salt = os.urandom(32)
-            secret = '{"node_system_id":{}, "tenant_id":{}, "secret":'.format(self.node_system_id, self.tenant_id) + self.generate_node_secret() + '}'
+            secret = '{"node_system_id":' + self.node_system_id + ', "tenant_id": ' + self.tenant_id + ', "secret":'+ self.generate_node_secret() + '}'
             encoded_secret = base64.b64encode(secret.encode())
             encoded_secret_string = encoded_secret.decode('utf-8')
             #print(type(encoded_secret))
