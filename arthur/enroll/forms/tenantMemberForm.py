dc04f71354abf75b1a0b5d2e9e7ff410e17a1972
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-03-23 22:34:27 -0400

    Adding support for multiple schemas

--- arthur/enroll/forms/tenantMemberForm.py
+++ arthur/enroll/forms/tenantMemberForm.py
@@ -7,13 +7,14 @@ from ..models import Tenant
 
 
 class TenantMemberForm(forms.Form):
-    member_name = forms.CharField(validators=[RegexValidator(r'[a-zA-Z ]+', 'Enter a valid name (only letters)')])
-    member_email = forms.CharField(validators=[RegexValidator(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", 'Enter a valid email')])
-    member_password = forms.CharField(validators=[RegexValidator(r'[a-zA-Z]+', 'Enter a valid password (only letters)')])
-    tenant = forms.ChoiceField(choices=[(tenant.tenant_name, tenant.tenant_name) for tenant in Tenant.objects().all()])
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+        member_name = forms.CharField(validators=[RegexValidator(r'[a-zA-Z ]+', 'Enter a valid name (only letters)')])
+        member_email = forms.CharField(validators=[RegexValidator(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)", 'Enter a valid email')])
+        member_password = forms.CharField(validators=[RegexValidator(r'[a-zA-Z]+', 'Enter a valid password (only letters)')])
+        tenant = forms.ChoiceField(choices=[(tenant.tenant_name, tenant.tenant_name) for tenant in Tenant.objects().all()])
 
         self.helper = FormHelper
         self.helper.form_method = 'post'
