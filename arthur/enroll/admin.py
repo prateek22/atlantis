90863beeed6353a896af418234ba5b7329f34636
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-03-23 21:48:46 -0400

    Adding support for multiple schemas

--- arthur/enroll/admin.py
+++ arthur/enroll/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import *
 
 # Register your models here.
-admin.site.register(EnrolledNode)
-admin.site.register(live_query)
-admin.site.register(dist_query_result)
+#admin.site.register(EnrolledNode)
+#admin.site.register(live_query)
+#admin.site.register(dist_query_result)
