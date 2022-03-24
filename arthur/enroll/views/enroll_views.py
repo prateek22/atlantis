542ba8b7cbaa9fae8b65a7708eb986d9e2d0604b
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-03-23 20:23:31 -0400

    Print enroll response for validation

--- arthur/enroll/views/enroll_views.py
+++ arthur/enroll/views/enroll_views.py
@@ -83,7 +83,7 @@ def enroll(request):
         node_key = host.update_node_address(address)
         response = ENROLL_RESPONSE
         response['node_key'] = node_key
-        print(dir(node_key))
+        print(node_key.to_python())
 
         return JsonResponse(response)
 
