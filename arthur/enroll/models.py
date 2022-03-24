723ef023d72c331c324e4ff0c8929904bf65c00b
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-03-23 21:47:09 -0400

    Adding support for multiple schemas

--- arthur/enroll/models.py
+++ arthur/enroll/models.py
@@ -5,13 +5,13 @@ from cassandra.cqlengine.models import Model
 from cassandra.cqlengine.usertype import UserType
 from django_cassandra_engine.models import DjangoCassandraModel
 
-class Tenant(DjangoCassandraModel):
+class Tenant(Model):
     tenant_id = columns.UUID(primary_key=True, default=uuid.uuid4)
     tenant_name = columns.Text(required=True)
     tenant_domain = columns.Text(required=True)
     #__type_name__ = 'Tenant'
 
-class TenantMember(DjangoCassandraModel):
+class TenantMember(Model):
     member_id = columns.UUID(primary_key=True, default=uuid.uuid4, partition_key=True)
     tenant_id = columns.UUID(required=True,partition_key=True) #columns.UserDefinedType(user_type=Tenant)
     member_name = columns.Text(required=True)
@@ -21,7 +21,7 @@ class TenantMember(DjangoCassandraModel):
     class Meta:
         get_pk_field = 'member_id'
 
-class EnrolledNode(DjangoCassandraModel):
+class EnrolledNode(Model):
     node_id = columns.UUID(primary_key=True, default=uuid.uuid4, partition_key=True)
     tenant_id = columns.UUID(required=True,partition_key=True, index=True) #columns.UserDefinedType(user_type=Tenant)
     node_system_id = columns.Text(required=True, index=True)
@@ -34,7 +34,7 @@ class EnrolledNode(DjangoCassandraModel):
     class Meta:
         get_pk_field = 'node_id'
 
-class alerts(DjangoCassandraModel):
+class alerts(Model):
 
     src_ip = columns.Text(max_length=50)
     src_port = columns.Text(max_length=50)
@@ -46,14 +46,14 @@ class alerts(DjangoCassandraModel):
     def __str__(self):
         return "{}:{} => {}:{}".format(self.src_ip, self.src_port, self.dest_ip, self.dest_port)
         
-class live_query(DjangoCassandraModel):
+class live_query(Model):
     query = columns.Text(max_length=250)
     query_id = columns.UUID(primary_key=True, default=uuid.uuid4, partition_key=True)
     
     def __str__(self):
       return self.query_id
       
-class dist_query_result(DjangoCassandraModel):
+class dist_query_result(Model):
     result_query = columns.Text(max_length=250)
     node_keys = columns.UUID(primary_key=True, default=uuid.uuid4, partition_key=True)
     
