5550fdee718935fd0185bb4fbd1ad55a84463ff3
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-03-23 22:35:42 -0400

    Adding support for multiple schemas

--- arthur/enroll/views/admin_views.py
+++ arthur/enroll/views/admin_views.py
@@ -19,7 +19,7 @@ def addTenantMember(request):
     if request.method == 'GET':
         form = TenantMemberForm()
         #form.helper.build_default_layout()
-        form.tenant.choices = [(tenant.tenant_name, tenant.tenant_name) for tenant in Tenant.objects().all()]
+        #form.tenant.choices = [(tenant.tenant_name, tenant.tenant_name) for tenant in Tenant.objects().all()]
         return render(request, 'enroll/administer/add_tenant_member.html', {'form': form})
     elif request.method == 'POST':
         form = TenantMemberForm(request.POST)
