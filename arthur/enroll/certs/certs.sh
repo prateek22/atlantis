21e14b53de85377ae592b221775e9ba2dada498f
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-03-24 03:40:01 -0400

    Adding support for multiple schemas

--- arthur/enroll/certs/certs.sh
+++ arthur/enroll/certs/certs.sh
@@ -5,7 +5,7 @@ openssl genrsa -out certs/$1.key.pem 2048
 
 openssl req -subj "/CN=$1" -extensions v3_req -sha256 -new -key certs/$1.key.pem -out certs/$1.csr
 
-openssl x509 -req -extensions v3_req -days 3650 -sha256 -in certs/$1.csr -CA certs/ca.pem -CAkey certs/ca.key.pem -CAcreateserial -out certs/$1.crt -extfile /usr/lib/ssl/openssl.cnf
+openssl x509 -req -extensions v3_req -days 3650 -sha256 -in certs/$1.csr -CA certs/ca.pem -CAkey certs/ca.key.pem -CAcreateserial -out certs/$1.crt -extfile /etc/pki/tls/openssl.cnf #/usr/lib/ssl/openssl.cnf
 
 cat certs/$1.crt certs/$1.key.pem > certs/$1-ca-full.pem
 
