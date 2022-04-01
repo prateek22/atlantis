6f7645f291420962e84da8b089be150f02e60df6
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-04-01 09:32:35 -0400

    Added template

--- arthur/arthur/settings.py
+++ arthur/arthur/settings.py
@@ -12,10 +12,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
-import environ
-
-ROOT_DIR = environ.Path(__file__) - 2
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
