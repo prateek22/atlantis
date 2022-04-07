a658e341c9038036a22ed6051fa4a29115af5f42
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-04-06 21:20:05 -0400

    Added custom user model for authentication

--- arthur/arthur/settings.py
+++ arthur/arthur/settings.py
@@ -187,3 +187,5 @@ STATICFILES_FINDERS = (
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'enroll.TenantMember'
