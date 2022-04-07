b84cd0c44b8bafcd918700c1c04858755a2bd5fc
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-04-07 02:57:22 -0400

    Added loginView for custom model

--- arthur/enroll/serializers.py
+++ arthur/enroll/serializers.py
@@ -35,7 +35,7 @@ class LoginSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=255, read_only=True)
     password = serializers.CharField(max_length=128, write_only=True)
     token = serializers.CharField(max_length=255, read_only=True)
-    tenant_id = serializers.UUIDField(max_length=36, read_only=True)
+    tenant_id = serializers.UUIDField(read_only=True)
 
     def validate(self, data):
         # The `validate` method is where we make sure that the current
