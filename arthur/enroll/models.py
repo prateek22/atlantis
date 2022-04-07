e1cb4b27a253f11a5a9e65bbc4f5b2e7f7a83cb0
Prateek Pradhan <pradhan.prateek22@gmail.com>
2022-04-06 22:04:51 -0400

    Added custom user model for authentication

--- arthur/enroll/models.py
+++ arthur/enroll/models.py
@@ -68,10 +68,10 @@ class TenantMember(AbstractBaseUser, PermissionsMixin):
     member_id = models.UUIDField(primary_key=True)
     tenant_id = models.UUIDField() #columns.UserDefinedType(user_type=Tenant)
     username = models.TextField(db_index=True, max_length=255, unique=True)
-    email = models.TextField(db_index=True, unique=True, default='a@tenant.com')
+    email = models.TextField(db_index=True, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
-    created_at = models.DateTimeField(auto_now_add=True, default=timezone.now)
+    created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     USERNAME_FIELD = 'email'
